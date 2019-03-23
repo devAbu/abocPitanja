5ea4c7cb2e10cc67a4ec2f1ad3a6edf161dab9da
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-03-23 17:37:10 +0100

    srni drugi zadatak....citanje poslanih podataka i ubacivanje u bazu

---
+++ back-end/zadatak2/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require ('connection/connect.php');
+
+$id = $_REQUEST["ID"];
+$feedback = $_REQUEST["text"];
+$session = $_REQUEST["session"];
+
+$sql = 'INSERT INTO aboc (`text`, `session`) VALUES ("$feedback", "$session") ';
+
+$exec = @mysqli_query($dbc, $sql);
+    if ($exec) {
+        echo '<ul><li>'.$id.'</li><li>'.$feedback.'</li><li>'.$session.'</li></ul>';
+    } else {
+        echo mysqli_error($dbc);
+    }
+
+?>
